7b3b50586a6601fa3445388c04fe9798f3cb3665
Lalita Khanna <lalita.khaana24@gmail.com>
2019-06-12 15:01:31 -0700

    Stream

---
+++ src/Tree_exp/Node.java
@@ -0,0 +1,17 @@
+package Tree_exp;
+
+public class Node {
+	int value;
+	Node left;
+	Node right;
+
+	public Node(int value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return   value +"";
+	}
+}
---
+++ src/com/staticblock/JavaHElloWOrld.java
@@ -0,0 +1,10 @@
+package com.staticblock;
+
+public class JavaHElloWOrld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing java hello world with Spring Boot Framework");
+	}
+
+}
